6b6dd305fcc636c92cf88504e0808623efc08a8f
ratelocation2023-create <ratelocation2023@gmail.com>
2026-08-14 14:45:56 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -6,10 +6,10 @@ const SHELL = [
   './',
   './index.html',
   './manifest.webmanifest',
-  './icons/icon-192.png',
-  './icons/icon-512.png',
-  './icons/maskable-512.png',
-  './icons/apple-touch-icon.png'
+  './icon-192.png',
+  './icon-512.png',
+  './maskable-512.png',
+  './apple-touch-icon.png'
 ];
 
 self.addEventListener('install', function (e) {
